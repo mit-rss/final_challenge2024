2d58301745ce5717fef51831f923c25036d263d1
Carlos <villac@mit.edu>
2024-04-29 20:13:43 +0000

    made package

---
+++ final_challenge2024/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'final_challenge2024'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='racecar',
+    maintainer_email='villac@mit.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
